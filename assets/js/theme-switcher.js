8a26b4e161c4b3e08197cd59f020b87d83d94869
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-01 20:53:44 +0300

    Update theme-switcher.js

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,9 +1,13 @@
-function changeTheme(){
-   const theme_switch = document.getElementById("theme-switch");
-   if (theme_switch.checked) {
-     DarkReader.enable();
-   }
-   else {
-     DarkReader.disable();
-   }
- }
+let icon = document.querySelector('label');
+let bool = document.querySelector('#Checkbox');
+let body = document.body;
+
+function changeTheme() {
+  if(bool.checked){
+    icon.innerHTML = "🌛";
+    body.style.setProperty('--bg', 'radial-gradient(circle, rgba(158,157,149,1) 0%, rgba(33,70,116,1) 100%)');
+  }else{
+    icon.innerHTML = "💥";
+    body.style.setProperty('--bg', 'radial-gradient(circle, rgba(255,239,115,1) 0%, rgba(115,177,255,1) 100%)');
+  }
+}
