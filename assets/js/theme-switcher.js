dab111bae8e418cb4b7212316835bda7306e76ab
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-01 17:24:57 +0300

    Add files via upload

---
+++ assets/js/theme-switcher.js
@@ -0,0 +1,9 @@
+ function changeTheme(){
+     const theme_switch = document.getElementById("theme-switch");
+     if (theme_switch.checked) {
+         DarkReader.enable();
+     }
+     else {
+         DarkReader.disable();
+     }
+ }
