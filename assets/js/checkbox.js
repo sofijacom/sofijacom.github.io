51f01b4fcb989e6b379dbf9cb0aab4dab965ed9f
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-01 21:24:15 +0300

    Update checkbox.js

--- assets/js/checkbox.js
+++ assets/js/checkbox.js
@@ -7,7 +7,7 @@ function changeTheme() {
         icon.innerHTML = "🌛";
         body.style.setProperty('--bg', 'radial-gradient(circle, rgba(158,157,149,1) 0%, rgba(33,70,116,1) 100%)');
     }else{
-        icon.innerHTML = "😎";
+        icon.innerHTML = "💥";
         body.style.setProperty('--bg', 'radial-gradient(circle, rgba(255,239,115,1) 0%, rgba(115,177,255,1) 100%)');
     }
 }
