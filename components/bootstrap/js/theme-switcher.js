607da45125359c1e7099a659dd9749223fdfd3bf
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-02 17:37:06 +0300

    Delete components/bootstrap/js/theme-switcher.js

--- components/bootstrap/js/theme-switcher.js
+++
@@ -1,9 +0,0 @@
- function changeTheme(){
-   const theme_switch = document.getElementById("theme-switch");
-   if (theme_switch.checked) {
-     DarkReader.enable();
-   }
-   else {
-     DarkReader.disable();
-   }
- }
