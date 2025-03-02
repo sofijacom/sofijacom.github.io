c40bb35a924c303c4cb220e4d4294dd4f84160a8
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-02 21:24:43 +0300

    Update theme-switcher.js

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,12 +1,10 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
     }    
 }
-
-toggleSwitch.addEventListener('change', switchTheme, false);
