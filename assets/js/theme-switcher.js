fdd33ce20c9fe007b2620969b94204f959429d7e
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-03 05:55:37 +0300

    Update theme-switcher.js

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -4,17 +4,17 @@ const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
-    if (currentTheme === 'dark') {
+    if (currentTheme === '/assets/style/_dark.scss') {
         toggleSwitch.checked = true;
     }
 }
 
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', '/assets/style/dark.scss');
+        document.documentElement.setAttribute('data-theme', '/assets/style/_dark.scss');
         localStorage.setItem('theme', '/assets/style/dark.scss');
     }
-    else {        document.documentElement.setAttribute('data-theme', '/assets/style/light.scss');
+    else {        document.documentElement.setAttribute('data-theme', '/assets/style/_light.scss');
           localStorage.setItem('theme', '/assets/style/light.scss');
     }    
 }
