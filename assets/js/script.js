323b34fde52d657fda8d1c78310dd44ad7be6bf8
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-09 14:49:50 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-
+  
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
     }
@@ -15,8 +15,8 @@ function switchTheme(e) {
         localStorage.setItem('theme', 'dark');
     }
     else {        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-    }
+          localStorage.setItem('theme', 'light');
+    }    
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
