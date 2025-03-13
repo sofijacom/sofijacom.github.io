5eebcbdd78eefb0d54d61e05fb06029b08fe9020
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-14 02:19:12 +0300

    Update theme-switcher.js

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,23 +1,23 @@
- function changeTheme(){
-   const theme_switch = document.getElementById("theme-switch");
-   if (theme_switch.checked) {
-     DarkReader.enable();
-     // Add the ff. line to write to memory.
-     localStorage.setItem("my-theme","dark");
-   }
-   else {
-     DarkReader.disable();
-     // Add the ff. line to write to memory.
-     localStorage.setItem("my-theme",null);
-   }
- }
+DarkReader.enable({
+    brightness: 100,
+    contrast: 90,
+    sepia: 10
+});
 
- // Check local storage every reload to know which theme to use.
- if (localStorage.getItem("my-theme")==="dark") {
-   // Use dark theme.
-   DarkReader.enable();
- }
- else {
-   // Use default theme.
-   DarkReader.disable();
- }
+DarkReader.disable();
+
+// Enable when the system color scheme is dark.
+DarkReader.auto({
+    brightness: 100,
+    contrast: 90,
+    sepia: 10
+});
+
+// Stop watching for the system color scheme.
+DarkReader.auto(false);
+
+// Get the generated CSS of Dark Reader returned as a string.
+const CSS = await DarkReader.exportGeneratedCSS();
+
+// Check if Dark Reader is enabled.
+const isEnabled = DarkReader.isEnabled();
