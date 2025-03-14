200783b4ec4a4965eab00ec651826ca6cd0f1716
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-14 10:02:06 +0300

    Update theme-switcher.js

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,23 +1,24 @@
+ // This function runs every time the switch is toggled.
  function changeTheme(){
    const theme_switch = document.getElementById("theme-switch");
    if (theme_switch.checked) {
-     DarkReader.enable();
+     document.documentElement.setAttribute("data-theme","dark");
      // Add the ff. line to write to memory.
      localStorage.setItem("my-theme","dark");
    }
    else {
-     DarkReader.disable();
+     document.documentElement.removeAttribute("data-theme")
      // Add the ff. line to write to memory.
      localStorage.setItem("my-theme",null);
    }
  }
 
- // Check local storage every reload to know which theme to use.
+ // Check local storage every time html is loaded to know which theme to use.
  if (localStorage.getItem("my-theme")==="dark") {
    // Use dark theme.
-   DarkReader.enable();
+   document.documentElement.setAttribute("data-theme","dark");
  }
  else {
    // Use default theme.
-   DarkReader.disable();
+   document.documentElement.removeAttribute("data-theme")
  }
