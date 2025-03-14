2d2499558f5684caa204725056ce80473025b08d
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-14 10:56:09 +0300

    Update theme-switcher.js

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,24 +1,23 @@
- // This function runs every time the switch is toggled.
  function changeTheme(){
    const theme_switch = document.getElementById("theme-switch");
    if (theme_switch.checked) {
-     document.documentElement.setAttribute("data-theme","dark");
+     DarkReader.enable();
      // Add the ff. line to write to memory.
      localStorage.setItem("my-theme","dark");
    }
    else {
-     document.documentElement.removeAttribute("data-theme")
+     DarkReader.disable();
      // Add the ff. line to write to memory.
      localStorage.setItem("my-theme",null);
    }
  }
 
- // Check local storage every time html is loaded to know which theme to use.
+ // Check local storage every reload to know which theme to use.
  if (localStorage.getItem("my-theme")==="dark") {
    // Use dark theme.
-   document.documentElement.setAttribute("data-theme","dark");
+   DarkReader.enable();
  }
  else {
    // Use default theme.
-   document.documentElement.removeAttribute("data-theme")
+   DarkReader.disable();
  }
