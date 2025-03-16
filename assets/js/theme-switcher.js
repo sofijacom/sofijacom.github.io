d6453377871a1c29c50a21f4c00a32417f3070c9
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-16 20:16:10 +0300

    Update theme-switcher.js

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,23 +1,53 @@
- function changeTheme(){
-   const theme_switch = document.getElementById("theme-switch");
-   if (theme_switch.checked) {
-     DarkReader.enable();
-     // Add the ff. line to write to memory.
-     localStorage.setItem("my-theme","dark");
-   }
-   else {
-     DarkReader.disable();
-     // Add the ff. line to write to memory.
-     localStorage.setItem("my-theme",null);
-   }
- }
+let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)"); 
+let theme = sessionStorage.getItem('theme');
 
- // Check local storage every reload to know which theme to use.
- if (localStorage.getItem("my-theme")==="dark") {
-   // Use dark theme.
-   DarkReader.enable();
- }
- else {
-   // Use default theme.
-   DarkReader.disable();
- }
+if (systemInitiatedDark.matches) {
+    document.getElementById("theme-toggle").innerHTML = '<i class="fa-solid fa-toggle-on"></i>';
+} else {
+    document.getElementById("theme-toggle").innerHTML = '<i class="fa-solid fa-toggle-off"></i>';
+}
+
+function prefersColorTest(systemInitiatedDark) {
+  if (systemInitiatedDark.matches) {
+  	document.documentElement.setAttribute('data-theme', 'dark');		
+    document.getElementById("theme-toggle").innerHTML = '<i class="fa-solid fa-toggle-on"></i>';
+   	sessionStorage.setItem('theme', '');
+  } else {
+  	document.documentElement.setAttribute('data-theme', 'light');
+    document.getElementById("theme-toggle").innerHTML = '<i class="fa-solid fa-toggle-off"></i>';
+    sessionStorage.setItem('theme', '');
+  }
+}
+systemInitiatedDark.addListener(prefersColorTest);
+
+
+function modeSwitcher() {
+	let theme = sessionStorage.getItem('theme');
+	if (theme === "dark") {
+		document.documentElement.setAttribute('data-theme', 'light');
+		sessionStorage.setItem('theme', 'light');
+        document.getElementById("theme-toggle").innerHTML = '<i class="fa-solid fa-toggle-off"></i>';
+	}	else if (theme === "light") {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		sessionStorage.setItem('theme', 'dark');
+        document.getElementById("theme-toggle").innerHTML = '<i class="fa-solid fa-toggle-on"></i>';
+	} else if (systemInitiatedDark.matches) {	
+		document.documentElement.setAttribute('data-theme', 'light');
+		sessionStorage.setItem('theme', 'light');
+        document.getElementById("theme-toggle").innerHTML = '<i class="fa-solid fa-toggle-off"></i>';
+	} else {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		sessionStorage.setItem('theme', 'dark');
+        document.getElementById("theme-toggle").innerHTML = '<i class="fa-solid fa-toggle-on"></i>';
+	}
+}
+
+if (theme === "dark") {
+	document.documentElement.setAttribute('data-theme', 'dark');
+	sessionStorage.setItem('theme', 'dark');
+    document.getElementById("theme-toggle").innerHTML = '<i class="fa-solid fa-toggle-on"></i>';
+} else if (theme === "light") {
+	document.documentElement.setAttribute('data-theme', 'light');
+	sessionStorage.setItem('theme', 'light');
+    document.getElementById("theme-toggle").innerHTML = '<i class="fa-solid fa-toggle-off"></i>';
+}
