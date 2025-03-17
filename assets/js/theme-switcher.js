12a8d77d66dfa75b3d2406ac910e7bbb632652c2
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-17 21:28:27 +0300

    Update theme-switcher.js

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,25 +1,22 @@
-let switches = document.getElementsByClassName('switch');
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
 
-let style = localStorage.getItem('style');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
 
-if (style == null) {
-    setTheme('light');
-} else {
-    setTheme(style);
-}
-
-for (let i of switches) {
-    i.addEventListener('click', function () {
-        let theme = this.dataset.theme;
-        setTheme(theme);
-    });
-}
-
-function setTheme(theme) {
-    if (theme == 'light') {
-        document.getElementById('switcher-id').href = 'sass/basically-basic/themes/plum.scss';
-    } else if (theme == 'dark') {
-        document.getElementById('switcher-id').href = 'sass/basically-basic/themes/macchiato.scss';
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
     }
-    localStorage.setItem('style', theme);
 }
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
