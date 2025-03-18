eb1796c9dfadb767ff1c31767122fb442db226a8
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-18 17:36:41 +0300

    Update theme-switcher.js

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,30 +1,23 @@
-// Выбираем кнопку
-const btn = document.querySelector("theme-switch");
-// Выбираем таблицу стилей
-const theme = document.querySelector("#theme-link");
-// Отслеживаем щелчок по кнопке
-btn.addEventListener("click", function() {
-    // Если текущий адрес содержит "light-theme.css"
-    if (theme.getAttribute("href") == "/sass/basically-basic/variables.scss") {
-        // …то переключаемся на "dark-theme.css"
-        theme.href = "/assets/css/dark-theme.css";
-        // В противном случае…
-        // Add the ff. line to write to memory.
-        localStorage.setItem("my-theme","dark");
-    } else {
-        // …переключаемся на "light-theme.css"
-        theme.href = "/sass/basically-basic/variables.scss";
-        // Add the ff. line to write to memory.
-        localStorage.setItem("my-theme",null);
-    }
-});
+ function changeTheme(){
+   const theme_switch = document.getElementById("theme-switch");
+   if (theme_switch.checked) {
+     DarkReader.enable();
+     // Add the ff. line to write to memory.
+     localStorage.setItem("my-theme","dark");
+   }
+   else {
+     DarkReader.disable();
+     // Add the ff. line to write to memory.
+     localStorage.setItem("my-theme",null);
+   }
+ }
 
-// Check local storage every reload to know which theme to use.
-if (localStorage.getItem("my-theme")==="dark") {
-    // Use dark theme.
-    theme.href = "/assets/css/dark-theme.css";
-}
-else {
-    // Use default theme.
-    theme.href = "/sass/basically-basic/variables.scss";
-}
+ // Check local storage every reload to know which theme to use.
+ if (localStorage.getItem("my-theme")==="dark") {
+   // Use dark theme.
+   DarkReader.enable();
+ }
+ else {
+   // Use default theme.
+   DarkReader.disable();
+ }
