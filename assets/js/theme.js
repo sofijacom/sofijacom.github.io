1de94078fbfda2018ce40f84996fb71a91c5dcd7
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-03-18 13:09:10 +0300

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -5,12 +5,12 @@ const theme = document.querySelector("#theme-link");
 // Отслеживаем щелчок по кнопке
 btn.addEventListener("click", function() {
     // Если текущий адрес содержит "light-theme.css"
-    if (theme.getAttribute("href") == "/assets/css/light-theme.css") {
+    if (theme.getAttribute("href") == "/sass/basically-basic/variables.scss") {
         // …то переключаемся на "dark-theme.css"
         theme.href = "/assets/css/dark-theme.css";
         // В противном случае…
     } else {
         // …переключаемся на "light-theme.css"
-        theme.href = "/assets/css/light-theme.css";
+        theme.href = "/sass/basically-basic/variables.scss";
     }
 });
