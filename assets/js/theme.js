2c6f5f8e3921530417c7958185e6ff084f954b88
sofijacom <sofija.p2018@gmail.com>
2025-03-19 14:30:18 +0300

    sofijacom.github.io

---
+++ assets/js/theme.js
@@ -0,0 +1,30 @@
+// Выбираем кнопку
+const btn = document.querySelector(".btn-toggle");
+// Выбираем таблицу стилей
+const theme = document.querySelector("#theme-link");
+// Отслеживаем щелчок по кнопке
+btn.addEventListener("click", function() {
+    // Если текущий адрес содержит "light-theme.css"
+    if (theme.getAttribute("href") == "/assets/css/light-theme.css") {
+        // …то переключаемся на "dark-theme.css"
+        theme.href = "/assets/css/dark-theme.css";
+        // В противном случае…
+             // Add the ff. line to write to memory.
+     localStorage.setItem("my-theme","dark");
+    } else {
+        // …переключаемся на "light-theme.css"
+        theme.href = "/assets/css/light-theme.css";
+             // Add the ff. line to write to memory.
+     localStorage.setItem("my-theme",null);
+    }    
+});
+
+     // Check local storage every reload to know which theme to use.
+ if (localStorage.getItem("my-theme")==="dark") {
+   // Use dark theme.
+   theme.href = "/assets/css/dark-theme.css";
+ }
+ else {
+   // Use default theme.
+   theme.href = "/assets/css/light-theme.css";
+ }
