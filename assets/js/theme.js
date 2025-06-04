0e89ab3eb0b8ad387a73def57470989b6f528f1a
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-04 14:47:09 +0300

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -30,11 +30,3 @@ themeToggle.addEventListener("click", function() {
    theme.href = "/assets/css/light-theme.css";
  }
 
-  const themeToggle = document.getElementById("theme-toggle");
-
-  if (typeof themeToggle !== "undefined") {
-    themeToggle.addEventListener("click", () =>
-      // Add timeout to wait for actual switch before changing theme.
-      setTimeout(changeGiscusTheme, 100),
-    );
-  }
