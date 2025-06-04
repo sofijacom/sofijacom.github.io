492642666a5dbe429bc2fc37d836a620d8662ff1
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-04 10:38:39 +0300

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -30,18 +30,3 @@ btn.addEventListener("click", function() {
    theme.href = "/assets/css/light-theme.css";
  }
 
-  function changeGiscusTheme () {
-    const theme = document.documentElement.getAttribute('data-theme') === 'dark' ?  'dark' : 'light'
-
-    function sendMessage(message) {
-      const iframe = document.querySelector('iframe.giscus-frame');
-      if (!iframe) return;
-      iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
-    }
-
-    sendMessage({
-      setConfig: {
-        theme: theme
-      }
-    });
-  }
