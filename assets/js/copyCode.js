5e9a6c030b93c1b56bce61475d0cf23db27498a4
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-09 13:59:52 +0300

    Add files via upload

---
+++ assets/js/copyCode.js
@@ -0,0 +1,15 @@
+const codeBlocks = document.querySelectorAll('.highlighter-rouge');
+const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+copyCodeButtons.forEach((copyCodeButton, index) => {
+    const code = codeBlocks[index].innerText;
+    copyCodeButton.addEventListener('click', () => {
+        // Скопировать код в буфер обмена пользователя
+        window.navigator.clipboard.writeText(code);
+        // Обновить текст кнопки
+        copyCodeButton.innerText = 'Copied!';
+        // (Опционально) добавить класс для стилизации кнопки
+        copyCodeButton.classList.add('copied');
+        // Через 2 секунды сбросить кнопку в исходное состояние
+        setTimeout(() => { copyCodeButton.innerText = originalText; copyCodeButton.classList.remove('copied'); }, 2000);
+    });
+});
