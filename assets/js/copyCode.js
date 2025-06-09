dc977628d1cc7871d0d01f225d2df45741b7e67f
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-09 15:48:10 +0300

    Update copyCode.js

--- assets/js/copyCode.js
+++ assets/js/copyCode.js
@@ -6,10 +6,10 @@ copyCodeButton.addEventListener('click', () => {
 // Скопировать код в буфер обмена пользователя
 window.navigator.clipboard.writeText(code);
 // Обновить текст кнопки
-copyCodeButton.innerText = 'copied';
+copyCodeButton.innerText = 'copied!';
 // (Опционально) добавить класс для стилизации кнопки
 copyCodeButton.classList.add('copied');
 // Через 2 секунды сбросить кнопку в исходное состояние
-setTimeout(() => { copyCodeButton.innerText = originalText; copyCodeButton.classList.remove('copied'); }, 2000);
+setTimeout(() => { copyCodeButton.innerText = originalText; copyCodeButton.classList.remove('copied'); }, 1000);
 });
 });
