c12c9adf938f005e9db8fae41d2fb75bb2539823
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-09 21:37:32 +0300

    Update copyCode.js

--- assets/js/copyCode.js
+++ assets/js/copyCode.js
@@ -1,25 +1,20 @@
-// This assumes that you're using Rouge; if not, update the selector
-const codeBlocks = document.querySelectorAll('.code-header + .highlighter-rouge');
-const copyCodeButtons = document.querySelectorAll('.copy-code-button');
-
-copyCodeButtons.forEach((copyCodeButton, index) => {
-  const code = codeBlocks[index].innerText;
-
-  copyCodeButton.addEventListener('click', () => {
-    // Copy the code to the user's clipboard
-    window.navigator.clipboard.writeText(code);
-
-    // Update the button text visually
-    const { innerText: originalText } = copyCodeButton;
-    copyCodeButton.innerText = 'Copied';
-
-    // (Optional) Toggle a class for styling the button
-    copyCodeButton.classList.add('copied');
-
-    // After 2 seconds, reset the button to its initial UI
-    setTimeout(() => {
-      copyCodeButton.innerText = originalText;
-      copyCodeButton.classList.remove('copied');
-    }, 2000);
-  });
-});
+document.querySelectorAll(".copy-code-button").forEach(e => {
+    let r = e.closest("figure.code-block")
+      , n = r?.querySelector("code")
+      , i = r?.querySelector('[role="alert"]');
+    !r || !n || e.addEventListener("click", () => {
+        window.navigator.clipboard.writeText(n.innerText);
+        let t = e.innerHTML;
+        e.classList.add("copied"),
+        e.innerText = "Copied",
+        i && (i.innerText = "Copied"),
+        setTimeout( () => {
+            e.classList.remove("copied"),
+            e.innerHTML = t,
+            i && (i.innerText = "")
+        }
+        , 2e3)
+    }
+    )
+}
+);
