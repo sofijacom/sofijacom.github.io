b257d2d34ffcc2bc227b6cb772ec35e2c426d8dc
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-09 14:18:02 +0300

    Update copyCode.js

--- assets/js/copyCode.js
+++ assets/js/copyCode.js
@@ -1,12 +1,12 @@
 const codeBlocks = document.querySelectorAll('.highlighter-rouge');
-const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+const copyCodeButtons = document.querySelectorAll('.copy-code');
 copyCodeButtons.forEach((copyCodeButton, index) => {
     const code = codeBlocks[index].innerText;
     copyCodeButton.addEventListener('click', () => {
         // Скопировать код в буфер обмена пользователя
         window.navigator.clipboard.writeText(code);
         // Обновить текст кнопки
-        copyCodeButton.innerText = 'Copied!';
+        copyCodeButton.innerText = 'Copy';
         // (Опционально) добавить класс для стилизации кнопки
         copyCodeButton.classList.add('copied');
         // Через 2 секунды сбросить кнопку в исходное состояние
