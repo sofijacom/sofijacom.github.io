94acb5b5825929ecf263317acab559005eb24786
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-10 09:34:38 +0300

    Update post.js

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,5 +1,5 @@
 // assets/js/post.js
-var codeBlocks = document.querySelectorAll('pre.highlight + #pre');
+var codeBlocks = document.querySelectorAll('pre.highlight');
 var codeBlocks = document.querySelector('#pre');
 codeBlocks.forEach(function (codeBlock) {
     var copyButton = document.createElement('button');
