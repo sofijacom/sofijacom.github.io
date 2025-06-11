30f00ab432e4d166db2e2da8d4ce685b9083795d
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-12 00:33:40 +0300

    Update post.js

--- assets/js/post.js
+++ assets/js/post.js
@@ -17,7 +17,11 @@ codeBlocks.forEach(function (codeBlock) {
         var code = codeBlock.querySelector('code').innerText.trim();
         window.navigator.clipboard.writeText(code);
         
-        copyButton.innerHTML = 'Copied <i class = "fa-solid fa-check"></i>';
+        copyButton.innerHTML = '<i class = "fa-solid fa-check"></i>';
+        clipboard.on('success', function(e) {
+    console.log(e);
+    alert('Copied!');
+});
         var twourSeconds = 2000;
 
         setTimeout(function () {
