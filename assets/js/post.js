262780e12d2f34d015b787e7827f06de5dec7ee7
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-11 11:48:25 +0300

    Update post.js

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,12 +1,38 @@
 // assets/js/post.js
+//var codeBlocks = document.querySelectorAll('pre.highlight');
+
+//codeBlocks.forEach(function (codeBlock) {
+//    var copyButton = document.createElement('button');
+//    copyButton.className = 'copy';
+//    copyButton.type = 'button';
+//    copyButton.ariaLabel = 'Copy code to clipboard';
+//    copyButton.innerText = 'Copy';
+
+//    codeBlock.append(copyButton);
+
+//    copyButton.addEventListener('click', function () {
+//        var code = codeBlock.querySelector('code').innerText.trim();
+//        window.navigator.clipboard.writeText(code);
+
+//        copyButton.innerText = 'Copied';
+//        var fourSeconds = 2000;
+
+//        setTimeout(function () {
+//            copyButton.innerText = 'Copy';
+//        }, fourSeconds);
+//    });
+//});
+
+// assets/js/post.js
 var codeBlocks = document.querySelectorAll('pre.highlight');
 
 codeBlocks.forEach(function (codeBlock) {
     var copyButton = document.createElement('button');
     copyButton.className = 'copy';
     copyButton.type = 'button';
+    copyButton.title = 'Copy';
     copyButton.ariaLabel = 'Copy code to clipboard';
-    copyButton.innerText = 'Copy';
+    copyButton.innerText = "📋";
 
     codeBlock.append(copyButton);
 
@@ -14,11 +40,11 @@ codeBlocks.forEach(function (codeBlock) {
         var code = codeBlock.querySelector('code').innerText.trim();
         window.navigator.clipboard.writeText(code);
 
-        copyButton.innerText = 'Copied';
+        copyButton.innerText = 'Copied ✔️';
         var fourSeconds = 2000;
 
         setTimeout(function () {
-            copyButton.innerText = 'Copy';
+            copyButton.innerClass = 'fa-regular fa-clone';
         }, fourSeconds);
     });
 });
