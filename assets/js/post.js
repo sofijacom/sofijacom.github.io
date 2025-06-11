dc89a118a63eedbf0d93e9f5313308cb44a55c29
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-11 16:34:27 +0300

    Update post.js

--- assets/js/post.js
+++ assets/js/post.js
@@ -34,8 +34,7 @@ codeBlocks.forEach(function (codeBlock) {
 //    copyButton.className = 'copy';
 //    copyButton.type = 'button';
 //    copyButton.ariaLabel = 'Copy code to clipboard';
-//    copyButton.title = 'Copy';
-//    copyButton.innerText = '📋';
+//    copyButton.innerText = 'Copy';
 
 //    codeBlock.append(copyButton);
 
@@ -43,11 +42,11 @@ codeBlocks.forEach(function (codeBlock) {
 //        var code = codeBlock.querySelector('code').innerText.trim();
 //        window.navigator.clipboard.writeText(code);
 
-//        copyButton.innerText = 'Copied ✔️';
+//        copyButton.innerText = 'Copied';
 //        var fourSeconds = 2000;
 
 //        setTimeout(function () {
-//            copyButton.innerText = "📋";
+//            copyButton.innerText = "Copy";
 //        }, fourSeconds);
 //    });
 // });
