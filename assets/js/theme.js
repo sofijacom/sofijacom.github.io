88d477884d724e8d9c9b6668985708829abe345f
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-13 20:01:44 +0300

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -9,27 +9,23 @@ themeToggle.addEventListener("click", function() {
     if (theme.getAttribute("href") == "/assets/css/light-theme.css") {
         // …то переключаемся на "dark-theme.css"
         theme.href = "/assets/css/dark-theme.css";
-        theme.href = '/_include/giscus-component.html';
         // В противном случае…
              // Add the ff. line to write to memory.
      localStorage.setItem("my-theme","dark");
     } else {
         // …переключаемся на "light-theme.css"
         theme.href = "/assets/css/light-theme.css";
-        {% include  giscus.html %};
              // Add the ff. line to write to memory.
      localStorage.setItem("my-theme",null);
     }    
 });
 
-     // Check local storage every reload to know which theme to use.
+     // Проверяйте локальное хранилище при каждой перезагрузке, чтобы знать, какую тему использовать.
  if (localStorage.getItem("my-theme")==="dark") {
    // Use dark theme.
    theme.href = "/assets/css/dark-theme.css";
-     theme.href = '/_include/giscus-component.html';
  }
  else {
    // Use default theme.
    theme.href = "/assets/css/light-theme.css";
-     {% include  giscus.html %};
  }
