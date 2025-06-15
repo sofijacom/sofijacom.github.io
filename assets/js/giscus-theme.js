dd123e3bc44e055af94741a000596ce060086ba3
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-15 16:41:37 +0300

    Update giscus-theme.js

--- assets/js/giscus-theme.js
+++ assets/js/giscus-theme.js
@@ -1,9 +1,19 @@
     document.getElementById("theme-toggle").addEventListener("click", () => {
         if (document.body.className.includes("dark")) {
             document.body.classList.remove('dark');
-           // localStorage.setItem("pref-theme", 'light');
+            localStorage.setItem("pref-theme", 'light');
         } else {
             document.body.classList.add('dark');
-           // localStorage.setItem("pref-theme", 'dark');
+            localStorage.setItem("pref-theme", 'dark');
         }
     })
+
+     // Проверяйте локальное хранилище при каждой перезагрузке, чтобы знать, какую тему использовать.
+ if (localStorage.getItem("pref-theme")==="dark") {
+   // Use dark theme.
+   document.body.classList.add('dark');
+ }
+ else {
+   // Use default theme.
+   document.body.classList.add('light');
+ }
