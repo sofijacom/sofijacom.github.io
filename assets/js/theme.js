5512b65f56dfd9b67bb9bcf18f73b2799be9e7a3
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-06-15 13:28:53 +0300

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,4 +1,38 @@
-    document.getElementById("theme-toggle").addEventListener("click", () => {
+// By Sofiya 
+// Выбираем кнопку
+const themeToggle = document.getElementById("theme-toggle");
+// Выбираем таблицу стилей
+const theme = document.querySelector("#theme-link");
+// Отслеживаем щелчок по кнопке
+themeToggle.addEventListener("click", function() {
+    // Если текущий адрес содержит "light-theme.css"
+    if (theme.getAttribute("href") == "/assets/css/light-theme.css") {
+        // …то переключаемся на "dark-theme.css"
+        theme.href = "/assets/css/dark-theme.css";
+        // В противном случае…
+             // Add the ff. line to write to memory.
+     localStorage.setItem("my-theme","dark");
+    } else {
+        // …переключаемся на "light-theme.css"
+        theme.href = "/assets/css/light-theme.css";
+             // Add the ff. line to write to memory.
+     localStorage.setItem("my-theme",null);
+    }    
+});
+
+     // Проверяйте локальное хранилище при каждой перезагрузке, чтобы знать, какую тему использовать.
+ if (localStorage.getItem("my-theme")==="dark") {
+   // Use dark theme.
+   theme.href = "/assets/css/dark-theme.css";
+ }
+ else {
+   // Use default theme.
+   theme.href = "/assets/css/light-theme.css";
+ }
+
+
+
+document.getElementById("theme-toggle").addEventListener("click", () => {
         if (document.body.className.includes("dark")) {
             document.body.classList.remove('dark');
             localStorage.setItem("pref-theme", 'light');
@@ -7,3 +41,4 @@
             localStorage.setItem("pref-theme", 'dark');
         }
     })
+
